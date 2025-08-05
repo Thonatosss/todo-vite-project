9ffafb27e489510120e5522b9a579708fd74a047
Mykola Romashenko <sven4369@gmail.com>
2025-08-05 13:07:47 +0300

    background gradient, shadows, title

--- src/modules/markup.js
+++ src/modules/markup.js
@@ -4,8 +4,8 @@ function createMarkup(arr) {
   const markup = arr
     .map(
       ({ id, text, isDone }) =>
-        `<li data-id=${id} data-editing="false" class="js-item rounded-xl bg-item-background w-65 h-55 flex flex-col items-center justify-between p-4 text-white"><div><p class="js-text text-lg text-center">${text}</p></div><div class="flex gap-4 justify-center items-center mt-20"><button class ="js-delete bg-accent-purple p-2 rounded-lg w-25 text-sm ">Delete task</button><button class="js-edit bg-accent-purple p-2 rounded-lg w-25 text-sm" >Edit</button>
-      <button class="js-save is-hidden bg-accent-purple p-2 rounded-lg w-25 text-sm">Save</button><button class="js-cancel is-hidden bg-accent-purple p-2 rounded-lg w-25 text-sm">Cancel</button> </div>
+        `<li data-id=${id} data-editing="false" class="js-item shadow-2xl shadow-black/20 rounded-xl bg-item-background w-65 h-55 flex flex-col items-center justify-between p-4 text-white"><div><p class="js-text text-lg text-center">${text}</p></div><div class="flex gap-4 justify-center items-center mt-20"><button class ="js-delete bg-accent-purple shadow-lg shadow-hover-purple/30 font-semibold p-2 rounded-lg w-25 text-sm ">Delete task</button><button class="js-edit bg-accent-purple shadow-lg shadow-hover-purple/30 font-semibold p-2 rounded-lg w-25 text-sm" >Edit</button>
+      <button class="js-save is-hidden bg-accent-purple shadow-lg shadow-hover-purple/30 font-semibold p-2 rounded-lg w-25 text-sm">Save</button><button class="js-cancel is-hidden bg-accent-purple shadow-lg shadow-hover-purple/30 font-semibold p-2 rounded-lg w-25 text-sm">Cancel</button> </div>
          </li>`
     )
     .join("");
